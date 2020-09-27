00bfb013c18cc0345a80c2a2c8b64c504b04e856
Bradley Chuter <30870299+bchuter@users.noreply.github.com>
2020-09-27 13:29:43 +1000

    Create xfce.sh

---
+++ xfce.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+printf "######   Installing vmware requirements   ######\n"
+sudo pacman -S --noconfirm xf86-input-vmmouse xf86-video-vmware mesa
+
+printf "######   Installing xorg   ######\n"
+sudo pacman -S --noconfirm xorg xorg-server
+
+printf "######   Installing xfce and a few extra apps   ######\n"
+sudo pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+
+printf "######   Enabling lightdm   ######\n"
+sudo systemctl enable lightdm
+
+printf "######   Installing pamac   ######\n"
+yay -S pamac-aur
+
+printf "######   Your setup is ready. You can reboot now!   ######\n"
