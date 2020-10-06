27849ebb19ef42d0c065cafd6efb492c5b714a9b
bchuter <30870299+bchuter@users.noreply.github.com>
2020-10-06 17:32:15 +1100

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+printf "######   Checking if post-install.sh has been run   ######\n"
+
+
+printf "######   Installing vmware requirements   ######\n"
+sudo pacman -S --noconfirm xf86-input-vmmouse xf86-video-vmware mesa
+
+printf "######   Installing xorg   ######\n"
+sudo pacman -S --noconfirm xorg xorg-server
+
+printf "######   Installing KDE   ######\n"
+sudo pacman -S --noconfirm plasma kde-applications sddm 
+
+printf "######   Enabling SDDM   ######\n"
+sudo systemctl enable sddm
+
+printf "######   Your setup is ready. You can reboot now!   ######\n"
