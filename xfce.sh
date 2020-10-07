2255433922d702218527da898ec7c2f5e3bfbefb
bchuter <30870299+bchuter@users.noreply.github.com>
2020-10-07 13:15:30 +1100

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -12,6 +12,9 @@ sudo pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter-setti
 printf "######   Enabling lightdm   ######\n"
 sudo systemctl enable lightdm 
 
+printf "######   Update yay   ######\n"
+yay -Syu --noconfirm
+
 printf "######   Installing tkpacman   ######\n"
 yay -S --noconfirm tkpacman
 
@@ -25,16 +28,27 @@ tee -a ~/.config/gtk-3.0/gtk.css << EOF
 }
 EOF
 
-printf "######   Create theme directories and staging folder   ######\n"
-mkdir ~/temp-staging ~/.icons ~/.themes
-
 printf "######   Downloading, Installing and Setting Cursors   ######\n"
-yay -S xcursor-breeze
+yay -S --noconfirm xcursor-breeze
 xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "Breeze"
 
 printf "######   Downloading, Installing and Setting Theme   ######\n"
 yay -S --noconfirm matcha-gtk-theme
 xfconf-query -c xsettings -p /Net/ThemeName  -s "Matcha-dark-azul"
+
+printf "######   Downloading, Installing and Setting Icons   ######\n"
+sudo pacman -S --noconfirm community/papirus-icon-theme
+xfconf-query -c xsettings -p /Net/IconThemeName  -s "Papirus-Dark"
+
+printf "######   Downloading, Installing and Setting Fonts   ######\n"
+sudo pacman -S --noconfirm extra/noto-fonts
+xfconf-query -c xsettings -p /Gtk/FontName  -s "Noto Sans 10"
+
+printf "######   Changing Panel Properties   ######\n"
+xfconf-query -c xfce4-panel -p /panels/panel-1/position -s "p=10;x=0;y=0"
+xfconf-query -c xfce4-panel -p /panels/panel-2/position -s "p=9;x=0;y=0"
+xfconf-query -c xfce4-panel -p /panels/panel-1/size -s "36" 
+xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -s "28"
 
 
 
