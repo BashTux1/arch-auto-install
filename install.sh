448cd03532375623db1088d7131b41ebe96bbc54
bchuter <30870299+bchuter@users.noreply.github.com>
2021-07-27 19:04:32 +1000

    bash format correction

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ### User Variables, Edit these for your environment
 ### Depricated. Script now reads input (see below) from user. 
@@ -17,68 +17,55 @@
 printf "\n>>>   Listing all Disk Devices\n\n"
 lsblk
 
-printf "\n>>>   This sets the [Disk Device] to use\n"
-printf "\nEnter Disk Device to Use [sda]: "
-read disk
+printf "\n>>>   This sets the [Disk Device] to use\n\n"
+read -r -p "Enter Disk Device to Use [sda]: " disk
 disk=${disk:-sda}
 
 printf "\n>>>   This sets the desired [EFI boot partition size]\n"
-printf ">>>   Must be in the format [300M] or [1G] etc\n"
-printf "\nEnter Boot Partition Size [300M]: "
-read boot
+printf ">>>   Must be in the format [300M] or [1G] etc\n\n"
+read -r -p "Enter Boot Partition Size [300M]: " boot
 boot=${boot:-300M}
 
 printf "\n>>>   This sets the desired [SWAP partition size]\n"
-printf ">>>   Must be in the format [300M] or [1G] etc\n"
-printf "\nEnter Swap Partition Size [16G]: "
-read swap
+printf ">>>   Must be in the format [300M] or [1G] etc\n\n"
+read -r -p "Enter Swap Partition Size [16G]: " swap
 swap=${swap:-16G}
 
-printf "\n>>>   This sets the desired [root password] for this new Arch install\n"
+printf "\n>>>   This sets the desired [root password] for this new Arch install\n\n"
 while true; do
-	printf "\nEnter the Root Password: "
-    read -s root_password1
-    echo
-	printf "\nConfirm the Root Password: "
-    read -s root_password
-    echo
-    [ "$root_password1" = "$root_password" ] && break
-    printf "\nPasswords DO NOT match, Please try again\n"
+	read -r -s -p "Enter the Root Password: " root_password1
+	read -r -s -p "Confirm the Root Password: " root_password
+
+	[ "${root_password1}" = "${root_password}" ] && break
+	printf "\nPasswords DO NOT match, Please try again\n"
 done
 
 printf "\n>>>   This sets the desired non-root [username] for this new Arch install.\n"
-printf ">>>   Note: The user will automatically be added to SUDOers\n"
-printf "\nEnter the User Name: "
-read user_name
+printf ">>>   Note: The user will automatically be added to SUDOers\n\n"
+read -r -p "Enter the User Name: " user_name
 
 while true; do
-	printf "\nEnter the User Password for [$user_name]: "
-    read -s user_password1
-    echo
-	printf "\nConfirm the User Password for [$user_name]: "
-    read -s user_password
-    echo
-    [ "$user_password1" = "$user_password" ] && break
-    printf "\nPasswords DO NOT match, Please try again\n"
+	read -r -s -p "Enter the User Password for [${user_name}]: " user_password1
+	read -r -s -p "Confirm the User Password for [${user_name}]: " user_password
+
+	[ "${user_password1}" = "${user_password}" ] && break
+	printf "\nPasswords DO NOT match, Please try again\n"
 done
 
-printf "\n>>>   This sets the system hostname\n"
-printf "\nEnter Hostname [arch]: "
-read hostname
+printf "\n>>>   This sets the system hostname\n\n"
+read -r -p "Enter Hostname [arch]: " hostname
 hostname=${hostname:-arch}
 
 printf "\n>>>   This sets the system Timezone\n"
 printf ">>>   Must have the following format: Zone/SubZone\n"
-printf ">>>   See: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones\n"
-printf "\nEnter the system Timezone [Australia/Sydney]: "
-read continent_city
+printf ">>>   See: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones\n\n"
+read -r -p "Enter the system Timezone [Australia/Sydney]: " continent_city
 continent_city=${continent_city:-Australia/Sydney}
 
 printf "\n>>>   This sets the country to be used by the [reflector] script\n"
 printf ">>>   Reflector is a Python script which can retrieve the most up-to-date package mirrors\n"
-printf ">>>   See: https://github.com/BashTux1/arch-auto-install/blob/master/README.md#reflector-country-list\n"
-printf "\nEnter Reflector Country [Australia]: "
-read reflector_country
+printf ">>>   See: https://github.com/BashTux1/arch-auto-install/blob/master/README.md#reflector-country-list\n\n"
+read -r -p "Enter Reflector Country [Australia]: " reflector_country
 reflector_country=${reflector_country:-Australia}
 
 ### End of Input Read 
@@ -87,26 +74,26 @@ printf "######   Updating system clock   ######\n"
 timedatectl set-ntp true
 
 printf "######   Applying Country Mirrors via reflector   ######\n"
-reflector --country $reflector_country --protocol http --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country "${reflector_country}" --protocol http --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
 printf "######   Syncing packages database   ######\n"
 pacman -Sy --noconfirm
 
 printf "######   Creating partition tables   ######\n"
-printf "n\n1\n\n+$boot\nef00\nw\ny\n" | gdisk /dev/$disk
-printf "n\n2\n\n+$swap\n8200\nw\ny\n" | gdisk /dev/$disk
-printf "n\n3\n\n\n8300\nw\ny\n" | gdisk /dev/$disk
+printf "n\n1\n\n+%s\nef00\nw\ny\n" "${boot}" | gdisk /dev/"${disk}"
+printf "n\n2\n\n+%s\n8200\nw\ny\n" "${swap}" | gdisk /dev/"${disk}"
+printf "n\n3\n\n\n8300\nw\ny\n" | gdisk /dev/"${disk}"
 
 printf "######   Formatting / partition and Mounting   ######\n"
-yes | mkfs.ext4 /dev/"$disk"3
-mount /dev/"$disk"3 /mnt
+yes | mkfs.ext4 /dev/"${disk}"3
+mount /dev/"${disk}"3 /mnt
 
 printf "######   Formatting /boot partition   ######\n"
-yes | mkfs.fat -F32 /dev/"$disk"1
+yes | mkfs.fat -F32 /dev/"${disk}"1
 
 printf "######   Enabling swap   ######\n"
-yes | mkswap /dev/"$disk"2
-swapon /dev/"$disk"2
+yes | mkswap /dev/"${disk}"2
+swapon /dev/"${disk}"2
 
 printf "######   Installing Arch Linux   ######\n"
 yes '' | pacstrap /mnt base base-devel linux linux-firmware e2fsprogs dosfstools networkmanager wget man-db man-pages nano vim dhcpcd open-vm-tools openssh grub efibootmgr os-prober git
@@ -117,19 +104,19 @@ genfstab -U /mnt >> /mnt/etc/fstab
 printf "######   Configuring new system   ######\n"
 arch-chroot /mnt /bin/bash <<EOF
 printf "######   Setting Time Zone   ######\n"
-ln -sf /usr/share/zoneinfo/$continent_city /etc/localtime
+ln -sf /usr/share/zoneinfo/"${continent_city}" /etc/localtime
 hwclock --systohc --localtime
 printf "######   Setting locales   ######\n"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 printf "######   Setting hostname   ######\n"
-echo $hostname > /etc/hostname
+echo "${hostname}" > /etc/hostname
 printf "######   Setting root password   ######\n"
-echo -en "$root_password\n$root_password" | passwd
+echo -en "${root_password}\n${root_password}" | passwd
 printf "######   Creating new user   ######\n"
-useradd -m -G wheel,video $user_name
-echo -en "$user_password\n$user_password" | passwd $user_name
+useradd -m -G wheel,video "${user_name}"
+echo -en "${user_password}\n${user_password}" | passwd "${user_name}"
 printf "######   Enabling DHCP   ######\n"
 systemctl enable dhcpcd
 printf "######   Enabling NetworkManager   ######\n"
@@ -142,7 +129,7 @@ printf "######   Adding user as a sudoer   ######\n"
 echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
 printf "######   Installing GRUB Bootloader   ######\n"
 mkdir /boot/EFI
-mount /dev/"$disk"1 /boot/EFI
+mount /dev/"${disk}"1 /boot/EFI
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
@@ -151,7 +138,7 @@ printf "######   Generating Hosts File   ######\n"
 tee -a /mnt/etc/hosts << EOF
 127.0.0.1	localhost
 ::1		localhost
-127.0.1.1	$hostname.localdomain	$hostname
+127.0.1.1	"${hostname}".localdomain	"${hostname}"
 EOF
 
 printf "######   Unmount Partions   ######\n"
@@ -161,26 +148,24 @@ printf "\n\n######   Arch Linux is ready. You can reboot now!   ######\n"
 
 ### This asks if you want to reboot
 printf "\n-------------------------------------\n"
-while true
-do
-    printf "\n>>>   Would you like to Reboot [Y]: "
-    read reboot
-    reboot=${reboot:-Y}
+while true; do
+	read -r -p ">>>   Would you like to Reboot [Y]: " reboot
+	reboot=${reboot:-Y}
 
-case $reboot in
-    [yY][eE][sS]|[yY])
-break
-;;
-    [nN][oO]|[nN])
- break
-    ;;
-    *)
- echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
- ;;
- esac
+	case $reboot in
+		[yY][eE][sS]|[yY])
+			break
+		;;
+		[nN][oO]|[nN])
+			break
+		;;
+		*)
+			echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
+		;;
+	esac
 done
 
-if [[ "$reboot" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "${reboot}" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-reboot
-fi 
+	reboot
+fi
--- post-install.sh
+++ post-install.sh
@@ -1,92 +1,88 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Check if user is root / sudo and if true, exit. "yay" install needs to run as normal user. 
 if [[ $(id -u) = 0 ]]
-  then printf "\nPlease run as non-root / sudo, ie. normal user\n\n"
-  exit 1
- fi
+	then printf "\nPlease run as non-root / sudo, ie. normal user\n\n"
+	exit 1
+fi
 
 printf "\n-------------------------------------\n"
 printf "\n>>>   This sets the country to be used by the [reflector] script\n"
 printf ">>>   Reflector is a Python script which can retrieve the most up-to-date package mirrors\n"
 printf ">>>   See: https://github.com/BashTux1/arch-auto-install/blob/master/README.md#reflector-country-list\n"
 printf "\nEnter Reflector Country [Australia]: "
-read reflector_country
+read -r reflector_country
 reflector_country=${reflector_country:-Australia}
 
 ### This asks if you want to install UFW and sets the variable accordingly
 printf "\n-------------------------------------\n"
 printf "\n>>>   The following will install [UFW (Uncomplicated Firewall)] and apply some default config\n"
 printf ">>>   Allow SSH (Port 22), Allow HTTP (Port 80) and Allow HTTPS (Port 443)\n"
-while true
-do
-    printf "\n>>>   Would you like to install UFW [Y]: "
-    read ufw
-    ufw=${ufw:-Y}
+while true; do
+	printf "\n>>>   Would you like to install UFW [Y]: "
+	read -r ufw
+	ufw=${ufw:-Y}
 
-case $ufw in
-    [yY][eE][sS]|[yY])
-break
-;;
-    [nN][oO]|[nN])
- break
-    ;;
-    *)
- echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
- ;;
- esac
+	case $ufw in
+		[yY][eE][sS]|[yY])
+			break
+		;;
+		[nN][oO]|[nN])
+			break
+		;;
+		*)
+			echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
+		;;
+	esac
 done
 
 ### This asks if you want to install Yay and sets the variable accordingly
 printf "\n-------------------------------------\n"
 printf "\n>>>   The following will install the widely used [yay] AUR Helper \n"
-while true
-do
-    printf "\n>>>   Would you like to install Yay [Y]: "
-    read yay
-    yay=${yay:-Y}
+while true; do
+	printf "\n>>>   Would you like to install Yay [Y]: "
+	read -r yay
+	yay=${yay:-Y}
 
-case $yay in
-    [yY][eE][sS]|[yY])
-break
-;;
-    [nN][oO]|[nN])
- break
-    ;;
-    *)
- echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
- ;;
- esac
+	case $yay in
+		[yY][eE][sS]|[yY])
+			break
+		;;
+		[nN][oO]|[nN])
+			break
+		;;
+		*)
+			echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
+		;;
+	esac
 done
 
 ### This asks if you want to install ZSH and sets the variable accordingly
 printf "\n-------------------------------------\n"
 printf "\n>>>   The following will install [Zsh] along with [Oh My Zsh] and apply some customisations\n"
-while true
-do
-    printf "\n>>>   Would you like to install Zsh [Y]: "
-    read zsh
-    zsh=${zsh:-Y}
+while true; do
+	printf "\n>>>   Would you like to install Zsh [Y]: "
+	read -r zsh
+	zsh=${zsh:-Y}
 
-case $zsh in
-    [yY][eE][sS]|[yY])
-break
-;;
-    [nN][oO]|[nN])
- break
-    ;;
-    *)
- echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
- ;;
- esac
+	case $zsh in
+		[yY][eE][sS]|[yY])
+			break
+		;;
+		[nN][oO]|[nN])
+			break
+		;;
+		*)
+			echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
+		;;
+	esac
 done
 
 ### If answered yes to install Zsh, ask for user passwords, required for the script
 if [[ "$zsh" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-printf "\n>>>   The password entry is required to make Zsh the default shell for User: $USER \n"
-printf "\nEnter Password for [$USER]: "
-read -s user_password
+	printf "\n>>>   The password entry is required to make Zsh the default shell for User: %s \n" "${USER}"
+	read -r -s -p "Enter Password for [${USER}]: " user_password
 fi
 
 printf "\n\n\n######   Syncing repos and updating packages   ######\n"
@@ -98,8 +94,7 @@ printf "######   Installing reflector and Applying Custom Mirrors   ######\n"
 sudo pacman -S --noconfirm reflector 
 
 sudo sed -i 's/^--/# --/g' /etc/xdg/reflector/reflector.conf
-sudo tee -a /etc/xdg/reflector/reflector.conf << EOF
-
+sudo tee -a /etc/xdg/reflector/reflector.conf << 'EOF'
 
 ##### Config Added via post-install.sh #####
 
@@ -116,16 +111,16 @@ sudo systemctl start reflector.service
 ### If answered yes, install UFW, else do not install
 if [[ "$ufw" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    printf "######   Installing and configuring UFW   ######\n"
-    sudo pacman -S --noconfirm ufw
-    sudo systemctl enable ufw
-    sudo systemctl start ufw
-    sudo ufw default deny incoming
-    sudo ufw default allow outgoing
-    sudo ufw limit ssh
-    sudo ufw allow 80
-    sudo ufw allow 443
-    echo "y" | sudo ufw enable
+	printf "######   Installing and configuring UFW   ######\n"
+	sudo pacman -S --noconfirm ufw
+	sudo systemctl enable ufw
+	sudo systemctl start ufw
+	sudo ufw default deny incoming
+	sudo ufw default allow outgoing
+	sudo ufw limit ssh
+	sudo ufw allow 80
+	sudo ufw allow 443
+	echo "y" | sudo ufw enable
 fi
 
 printf "######   Installing common applications   ######\n"
@@ -134,12 +129,12 @@ sudo pacman -S --noconfirm htop p7zip ripgrep unzip unrar
 ### If answered yes, install yay, else do not install
 if [[ "$yay" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-printf "######   Installing yay   ######\n"
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay-bin
+	printf "######   Installing yay   ######\n"
+	git clone https://aur.archlinux.org/yay.git
+	cd yay || exit
+	makepkg -si --noconfirm
+	cd ..
+	rm -rf yay-bin
 fi
 
 # printf "######   Installing fonts   ######\n"
@@ -148,7 +143,7 @@ fi
 printf "######   Ricing bash   ######\n"
 mv ~/.bashrc .bashrc_original
 touch ~/.bashrc
-tee -a ~/.bashrc << EOF
+tee -a ~/.bashrc << 'EOF'
 #
 # ~/.bashrc
 #
@@ -172,33 +167,31 @@ alias free='free -m'                      # show sizes in MB
 alias more=less
 
 complete -cf sudo
-
 EOF
 
 ### If answered yes, install Zsh, else do not install
 if [[ "$zsh" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-printf "######   Installing Zsh   ######\n"
-sudo pacman -S --noconfirm zsh zsh-completions
+	printf "######   Installing Zsh   ######\n"
+	sudo pacman -S --noconfirm zsh zsh-completions
 
-printf "######   Installing 'oh my zsh'   ######\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+	printf "######   Installing 'oh my zsh'   ######\n"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-printf "######   Making Zsh the default shell   ######\n"
-echo $user_password | chsh -s /bin/zsh
-while [ $? -eq 1 ]
-do
-	printf "You entered the incorrect password for user: $USER, try again!\n"
-	chsh -s /bin/zsh
-done
+	printf "######   Making Zsh the default shell   ######\n"
+	echo "${user_password}" | chsh -s /bin/zsh
+		while [ $? -eq 1 ]; do
+			printf "You entered the incorrect password for user: %s, try again! \n" "${USER}"
+			chsh -s /bin/zsh
+		done
 
 printf "######   Installing Custom Zsh plugins   ######\n"
 ## zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 ## zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 ## zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-history-substring-search
 
 printf "######   Enabling Custom Zsh plugins   ######\n" 
 sudo sed -i 's/^plugins=(git)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search)/g' ~/.zshrc
@@ -207,8 +200,7 @@ printf "######   Changing Zsh default theme   ######\n"
 sudo sed -i 's/^ZSH_THEME="robbyrussell"/ZSH_THEME="lukerandall"/g' ~/.zshrc
 
 printf "######   Adding Zsh key bindings for Keypad   ######\n"
-tee -a ~/.zshrc << EOF
-
+tee -a ~/.zshrc << 'EOF'
 ## Keypad Bindings
 
 ## The actual codes (for example ^[Oq) may be different on your system. 
@@ -258,23 +250,22 @@ printf "######   If you installed Zsh, Log out and in (or reboot), for shell cha
 
 ### This asks if you want to reboot
 printf "\n-------------------------------------\n"
-while true
-do
-    printf "\n>>>   Would you like to Reboot [Y]: "
-    read reboot
-    reboot=${reboot:-Y}
+while true; do
+	printf "\n>>>   Would you like to Reboot [Y]: "
+	read -r reboot
+	reboot=${reboot:-Y}
 
-case $reboot in
-    [yY][eE][sS]|[yY])
-break
-;;
-    [nN][oO]|[nN])
- break
-    ;;
-    *)
- echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
- ;;
- esac
+	case $reboot in
+		[yY][eE][sS]|[yY])
+			break
+		;;
+		[nN][oO]|[nN])
+			break
+		;;
+		*)
+			echo ">>>   Invalid input... Valid entries are E.g. [Y / N] or [y / n] or [Yes / No]"
+		;;
+	esac
 done
 
 if [[ "$reboot" =~ ^([yY][eE][sS]|[yY])$ ]]
